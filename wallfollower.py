17b8decfad3848eda7f0c72ebf5a221c5217e7a6
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-14 11:38:59 -0600

    wall following

---
+++ wallfollower.py
@@ -0,0 +1,38 @@
+import setup
+import RoboPiLib as RPL
+import time
+
+now = time.time()
+
+front = 23
+front = 16
+
+RPL.servoWrite(0, 2000)
+RPL.servoWrite(2, 1000)
+while True:
+    if RPL.digitalRead(16) == 0:
+        if RPL.digitalRead(23) == 0:
+            future = time.time() + 2
+            RPL.servoWrite(0, 0)
+            RPL.servoWrite(2, 0)
+            while time.time() < future:
+                RPL.servoWrite(0, 2000)
+                if time.time() >= future:
+                    RPL.servoWrite(0, 2000)
+                    RPL.servoWrite(2, 1000)
+        elif RPL.digitalRead(23) == 1:
+            RPL.servoWrite(0, 2000)
+            RPL.servoWrite(2, 1000)
+    elif RPL.digitalRead(16) == 1:
+        if RPL.digitalRead(23) == 0:
+            future = time.time() + 2
+            RPL.servoWrite(0, 0)
+            RPL.servoWrite(2, 0)
+            while time.time() < future:
+                RPL.servoWrite(0, 2000)
+                if time.time() >= future:
+                    RPL.servoWrite(0, 2000)
+                    RPL.servoWrite(2, 1000)
+        elif RPL.digitalRead(23) == 1:
+            RPL.servoWrite(0, 2000)
+            RPL.servoWrite(2, 1000)
