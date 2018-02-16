f22212c72abe47f4b4eefbb6c1a3949d3c670c23
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-16 10:21:14 -0600

    trying to stop it

--- wallfollower.py
+++ wallfollower.py
@@ -3,6 +3,7 @@ import RoboPiLib as RPL
 import time
 
 now = time.time()
+future = now
 
 front = 23
 front = 16
@@ -13,9 +14,9 @@ print "going original"
 while True:
     if RPL.digitalRead(16) == 0:
         if RPL.digitalRead(23) == 0:
-            future = time.time() + 2
             RPL.servoWrite(0, 0)
             RPL.servoWrite(2, 0)
+            future = time.time() + 2
             print "stopping 1"
             while time.time() < future:
                 RPL.servoWrite(0, 2000)
