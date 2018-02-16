5f0c5a2ceb22a77f252bc6aa65aab01754b6b279
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-16 10:16:46 -0600

    printing better stuff

--- wallfollower.py
+++ wallfollower.py
@@ -9,38 +9,39 @@ front = 16
 
 RPL.servoWrite(0, 2000)
 RPL.servoWrite(2, 1000)
+print "going original"
 while True:
     if RPL.digitalRead(16) == 0:
         if RPL.digitalRead(23) == 0:
             future = time.time() + 2
             RPL.servoWrite(0, 0)
             RPL.servoWrite(2, 0)
-            print "stopping"
+            print "stopping 1"
             while time.time() < future:
                 RPL.servoWrite(0, 2000)
-                print "turning"
+                print "turning 1"
                 if time.time() >= future:
                     RPL.servoWrite(0, 2000)
                     RPL.servoWrite(2, 1000)
-                    print "going"
+                    print "going 1"
         elif RPL.digitalRead(23) == 1:
             RPL.servoWrite(0, 2000)
             RPL.servoWrite(2, 1000)
-            print "going"
+            print "continues"
     elif RPL.digitalRead(16) == 1:
         if RPL.digitalRead(23) == 0:
             future = time.time() + 2
             RPL.servoWrite(0, 0)
             RPL.servoWrite(2, 0)
-            print "stopping"
+            print "stopping 2"
             while time.time() < future:
                 RPL.servoWrite(0, 2000)
-                print "turning"
+                print "turning 2"
                 if time.time() >= future:
                     RPL.servoWrite(0, 2000)
                     RPL.servoWrite(2, 1000)
-                    print "going"
+                    print "going 2"
         elif RPL.digitalRead(23) == 1:
             RPL.servoWrite(0, 2000)
             RPL.servoWrite(2, 1000)
-            print "going"
+            print "continues 2"
