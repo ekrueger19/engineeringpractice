33a986131738d2ee91fd18d34493c49c760337c2
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-16 10:15:25 -0600

    printing stuff to see the problem

--- wallfollower.py
+++ wallfollower.py
@@ -15,24 +15,32 @@ while True:
             future = time.time() + 2
             RPL.servoWrite(0, 0)
             RPL.servoWrite(2, 0)
+            print "stopping"
             while time.time() < future:
                 RPL.servoWrite(0, 2000)
+                print "turning"
                 if time.time() >= future:
                     RPL.servoWrite(0, 2000)
                     RPL.servoWrite(2, 1000)
+                    print "going"
         elif RPL.digitalRead(23) == 1:
             RPL.servoWrite(0, 2000)
             RPL.servoWrite(2, 1000)
+            print "going"
     elif RPL.digitalRead(16) == 1:
         if RPL.digitalRead(23) == 0:
             future = time.time() + 2
             RPL.servoWrite(0, 0)
             RPL.servoWrite(2, 0)
+            print "stopping"
             while time.time() < future:
                 RPL.servoWrite(0, 2000)
+                print "turning"
                 if time.time() >= future:
                     RPL.servoWrite(0, 2000)
                     RPL.servoWrite(2, 1000)
+                    print "going"
         elif RPL.digitalRead(23) == 1:
             RPL.servoWrite(0, 2000)
             RPL.servoWrite(2, 1000)
+            print "going"
