e8838af15ceb0c7f28368f76dc34e4dd0c83f397
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-26 10:14:10 -0600

    consolidated

--- evading.py
+++ evading.py
@@ -12,37 +12,18 @@ RPL.servoWrite(0, 2000)
 RPL.servoWrite(2, 1000)
 print "going original"
 while True:
-    if RPL.digitalRead(16) == 0:
-        if RPL.digitalRead(23) == 0:
-            RPL.servoWrite(0, 0)
-            RPL.servoWrite(2, 0)
-            future = time.time() + 2
-            print "stopping 1"
+    while RPL.digitalRead(16) == 0:
+        while time.time() < future:
+            RPL.servoWrite(2, 2000)
+            if time.time() >= future:
+                RPL.servoWrite(0, 1000)
+                RPL.servoWrite(2, 2000)
+    while RPL.digitalRead(16) == 1:
+        RPL.servoWrite(0, 2000)
+        RPL.servoWrite(2, 1000)
+        if RPL.digitalRead(16) == 0:
             while time.time() < future:
                 RPL.servoWrite(2, 2000)
-                print "turning 1"
                 if time.time() >= future:
                     RPL.servoWrite(0, 1000)
                     RPL.servoWrite(2, 2000)
-                    print "going 1"
-        elif RPL.digitalRead(23) == 1:
-            RPL.servoWrite(0, 1000)
-            RPL.servoWrite(2, 2000)
-            print "continues"
-    elif RPL.digitalRead(16) == 1:
-        if RPL.digitalRead(23) == 0:
-            future = time.time() + 2
-            RPL.servoWrite(0, 0)
-            RPL.servoWrite(2, 0)
-            print "stopping 2"
-            while time.time() < future:
-                RPL.servoWrite(2, 2000)
-                print "turning 2"
-                if time.time() >= future:
-                    RPL.servoWrite(0, 1000)
-                    RPL.servoWrite(2, 2000)
-                    print "going 2"
-        elif RPL.digitalRead(23) == 1:
-            RPL.servoWrite(0, 1000)
-            RPL.servoWrite(2, 2000)
-            print "continues 2"
