e92c786dd98cf69e33f2fc906f51589285dd7bb5
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-26 10:19:01 -0600

    motors asd;gkhvserlghiselirgjs

--- evading.py
+++ evading.py
@@ -8,8 +8,8 @@ future = now
 front = 23
 front = 16
 
-RPL.servoWrite(0, 2000)
-RPL.servoWrite(2, 1000)
+RPL.servoWrite(0, 1000)
+RPL.servoWrite(2, 2000)
 print "going original"
 while True:
     while RPL.digitalRead(16) == 0:
@@ -19,8 +19,8 @@ while True:
                 RPL.servoWrite(0, 1000)
                 RPL.servoWrite(2, 2000)
     while RPL.digitalRead(16) == 1:
-        RPL.servoWrite(0, 2000)
-        RPL.servoWrite(2, 1000)
+        RPL.servoWrite(0, 1000)
+        RPL.servoWrite(2, 2000)
         if RPL.digitalRead(16) == 0:
             while time.time() < future:
                 RPL.servoWrite(2, 2000)
