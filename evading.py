c5d8274056d72237c89a8c8f0aa9ea5265d5ccf8
Amelia Backes <abackes19@bsmschool.org>
2018-02-27 10:02:17 -0600

    bc it wants me to

--- evading.py
+++ evading.py
@@ -13,22 +13,18 @@ RPL.servoWrite(2, 1000)
 print "going original"
 while True:
     if RPL.digitalRead(16) == 0:
-        if RPL.digitalRead(23) == 0:
-            RPL.servoWrite(0, 0)
-            RPL.servoWrite(2, 0)
-            future = time.time() + 2
-            print "stopping 1"
-            while time.time() < future:
-                RPL.servoWrite(2, 2000)
-                print "turning 1"
-                if time.time() >= future:
-                    RPL.servoWrite(0, 1000)
-                    RPL.servoWrite(2, 2000)
-                    print "going 1"
-        elif RPL.digitalRead(23) == 1:
-            RPL.servoWrite(0, 1000)
+        RPL.servoWrite(0, 0)
+        RPL.servoWrite(2, 0)
+        future = time.time() + 2
+        print "stopping 1"
+        while time.time() < future:
             RPL.servoWrite(2, 2000)
-            print "continues"
+            print "turning 1"
+            if time.time() >= future:
+                RPL.servoWrite(0, 1000)
+                RPL.servoWrite(2, 2000)
+                print "going 1"
+
     elif RPL.digitalRead(16) == 1:
         if RPL.digitalRead(23) == 0:
             future = time.time() + 2
