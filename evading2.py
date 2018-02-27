725c59f05884e84075180ab12c87a22011774396
Amelia Backes <abackes19@bsmschool.org>
2018-02-27 10:01:26 -0600

    time got cut

--- evading2.py
+++ evading2.py
@@ -10,34 +10,34 @@ front = 16
 
 RPL.servoWrite(0, 2000)
 RPL.servoWrite(2, 1000)
-print "going original"
 while True:
     if RPL.digitalRead(16) == 0:
         RPL.servoWrite(0, 0)
         RPL.servoWrite(2, 0)
-        future = time.time() + 2
         print "stopping 1"
-        while time.time() < future:
+        while RPL.digitalRead(16) == 0:
             RPL.servoWrite(2, 2000)
             print "turning 1"
-            if time.time() >= future:
+            if RPL.digitalRead(16) != 0:
                 RPL.servoWrite(0, 1000)
                 RPL.servoWrite(2, 2000)
+                break
                 print "going 1"
+
 
     elif RPL.digitalRead(16) == 1:
         if RPL.digitalRead(23) == 0:
-            future = time.time() + 2
             RPL.servoWrite(0, 0)
             RPL.servoWrite(2, 0)
-            print "stopping 2"
-            while time.time() < future:
+            print "stopping 1"
+            while RPL.digitalRead(23) == 0:
                 RPL.servoWrite(2, 2000)
-                print "turning 2"
-                if time.time() >= future:
+                print "turning 1"
+                if RPL.digitalRead(23) != 0:
                     RPL.servoWrite(0, 1000)
                     RPL.servoWrite(2, 2000)
-                    print "going 2"
+                    break
+                    print "going 1"
         elif RPL.digitalRead(23) == 1:
             RPL.servoWrite(0, 1000)
             RPL.servoWrite(2, 2000)
