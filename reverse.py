8c007d67acb0177335e18919136f841827d4b471
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-28 10:27:08 -0600

    wrote the code

--- reverse.py
+++ reverse.py
@@ -1,3 +1,57 @@
 #If all three sensors are 0, stop then back up then turn 90 degrees then go
 import setup
-import RoboPiLib
+import RoboPiLib as RPL
+import time
+
+now = time.time()
+future = now
+
+while RPL.digitalRead(16) == 1:
+    RPL.servoWrite(0, 1000)
+    RPL.servoWrite(2, 2000)
+    while RPL.digitalRead(16) == 0:
+        while RPL.digitalRead(23) == 0:
+            while RPL.digitalRead(17) == 0:
+                RPL.servoWrite(0, 2000)
+                RPL.servoWrite(2, 1000)
+                while RPL.digitalRead(16) == 1:
+                    while RPL.digitalRead(23) == 1:
+                        future = time.time() + 2
+                        while time.time() < future:
+                            RPL.servoWrite(0, 1000)
+                            RPL.servoWrite(2, 0)
+                            if time.time() >= future:
+                                RPL.servoWrite(0, 1000)
+                                RPL.servoWrite(2, 2000)
+                    while RPL.digitalRead(23) == 0:
+                        while RPL.digitalRead(17) == 1:
+                            future = time.time() + 2
+                            while time.time() < future:
+                                RPL.servoWrite(2, 2000)
+                                RPL.servoWrite(0, 0)
+                                if time.time() >= future:
+                                    RPL.servoWrite(0, 1000)
+                                    RPL.servoWrite(2, 2000)
+while RPL.digitalRead(16) == 0:
+    while RPL.digitalRead(23) == 0:
+        while RPL.digitalRead(17) == 0:
+            RPL.servoWrite(0, 2000)
+            RPL.servoWrite(2, 1000)
+            while RPL.digitalRead(16) == 1:
+                while RPL.digitalRead(23) == 1:
+                    future = time.time() + 2
+                    while time.time() < future:
+                        RPL.servoWrite(0, 1000)
+                        RPL.servoWrite(2, 0)
+                        if time.time() >= future:
+                            RPL.servoWrite(0, 1000)
+                            RPL.servoWrite(2, 2000)
+                while RPL.digitalRead(23) == 0:
+                    while RPL.digitalRead(17) == 1:
+                        future = time.time() + 2
+                        while time.time() < future:
+                            RPL.servoWrite(2, 2000)
+                            RPL.servoWrite(0, 0)
+                            if time.time() >= future:
+                                RPL.servoWrite(0, 1000)
+                                RPL.servoWrite(2, 2000)
