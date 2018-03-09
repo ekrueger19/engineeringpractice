0e31d334a61e28d4779616024f415cf0f4fcd887
Amelia Backes <abackes19@bsmschool.org>
2018-03-09 10:12:33 -0600

    switched 2000 and 1000

--- evading2.py
+++ evading2.py
@@ -12,27 +12,27 @@ right = 23
 front = 16
 
 
-RPL.servoWrite(motorR, 1000)
-RPL.servoWrite(motorR, 1000)
-RPL.servoWrite(motorL, 2000)
-RPL.servoWrite(motorL, 2000)
+RPL.servoWrite(motorR, 2000)
+RPL.servoWrite(motorR, 2000)
+RPL.servoWrite(motorL, 1000)
+RPL.servoWrite(motorL, 1000)
 while True:
-    RPL.servoWrite(motorR, 1000)
-    RPL.servoWrite(motorL, 2000)
+    RPL.servoWrite(motorR, 2000)
+    RPL.servoWrite(motorL, 1000)
     print ".............."
     while RPL.digitalRead(front) == 0: # ... turn until nothing
-        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorL, 2000)
         print "++++++"
         if RPL.digitalRead(front) != 0: # nothing in front, go
-            RPL.servoWrite(motorR, 1000)
-            RPL.servoWrite(motorL, 2000)
+            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorL, 1000)
             print "============="
             break
     while RPL.digitalRead(right) == 0: # something to right...
         print "llllllllllllll"
         RPL.servoWrite(motorL, 2000)
         if RPL.digitalRead(right) != 0: # nothing to side, go
-            RPL.servoWrite(motorR, 1000)
-            RPL.servoWrite(motorL, 2000)
+            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorL, 1000)
             print ":::::::::::"
             break
