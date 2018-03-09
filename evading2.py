6e575668972bcf20a126e0af205e2e8764d9dbd2
Amelia Backes <abackes19@bsmschool.org>
2018-03-09 10:04:44 -0600

    done got rid of ifs

--- evading2.py
+++ evading2.py
@@ -5,34 +5,38 @@ import time
 now = time.time()
 future = now
 
-front = 23
+motorL = 2
+motorR = 0
+
+right = 23
 front = 16
 
-RPL.servoWrite(0, 1000)
-RPL.servoWrite(0, 1000)
-RPL.servoWrite(2, 2000)
-RPL.servoWrite(2, 2000)
+def turn(future)
+
+
+
+
+RPL.servoWrite(motorR, 1000)
+RPL.servoWrite(motorR, 1000)
+RPL.servoWrite(motorL, 2000)
+RPL.servoWrite(motorL, 2000)
 while True:
-    if RPL.digitalRead(16) == 0:
-        print "++++++++++++++++"
-        while RPL.digitalRead(16) == 0:
-            RPL.servoWrite(2, 1000)
-            if RPL.digitalRead(16) != 0:
-                RPL.servoWrite(0, 1000)
-                RPL.servoWrite(2, 2000)
-                print "============="
-                break
-    elif RPL.digitalRead(16) == 1:
-        if RPL.digitalRead(23) == 0:
-            print "llllllllllllll"
-            while RPL.digitalRead(23) == 0:
-                RPL.servoWrite(2, 2000)
-                if RPL.digitalRead(23) != 0:
-                    RPL.servoWrite(0, 1000)
-                    RPL.servoWrite(2, 2000)
-                    print ":::::::::::"
-                    break
-        elif RPL.digitalRead(23) == 1:
-            RPL.servoWrite(0, 1000)
-            RPL.servoWrite(2, 2000)
-            print ".............."
+    RPL.servoWrite(motorR, 1000)
+    RPL.servoWrite(motorL, 2000)
+    print ".............."
+    while RPL.digitalRead(front) == 0: # ... turn until nothing
+        RPL.servoWrite(motorL, 1000)
+        print "++++++"
+        if RPL.digitalRead(front) != 0: # nothing in front, go
+            RPL.servoWrite(motorR, 1000)
+            RPL.servoWrite(motorL, 2000)
+            print "============="
+            break
+    while RPL.digitalRead(right) == 0: # something to right...
+        print "llllllllllllll"
+        RPL.servoWrite(motorL, 2000)
+        if RPL.digitalRead(right) != 0: # nothing to side, go
+            RPL.servoWrite(motorR, 1000)
+            RPL.servoWrite(motorL, 2000)
+            print ":::::::::::"
+            break
