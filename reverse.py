b0c26707c803f6cf33bb952b68f82583d431d27a
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-03-12 10:13:51 -0500

    indentations

--- reverse.py
+++ reverse.py
@@ -6,7 +6,7 @@ import time
 now = time.time()
 future = now
 
-while RPL.digitalRead(16) == 1: # nothing in front
+while RPL.digitalRead(16) == 1:
     RPL.servoWrite(0, 2000)
     RPL.servoWrite(2, 1000)
     while RPL.digitalRead(16) == 0:
@@ -16,32 +16,32 @@ while RPL.digitalRead(16) == 1: # nothing in front
             RPL.servoWrite(2, 2000)
             if time.time() >= future:
                 while RPL.digitalRead(23) == 0:
-                while RPL.digitalRead(17) == 0:
-                    future = time.time() + 3
-                    while time.time() < future:
-                        RPL.servoWrite(0, 1000) # motors reverse
-                        RPL.servoWrite(2, 2000)
-                        if time.time() >= future:
-                            RPL.servoWrite(0, 0)
-                            RPL.servoWrite(2, 0)
-                            while RPL.digitalRead(16) == 1:
-                                while RPL.digitalRead(23) == 1:
+                    while RPL.digitalRead(17) == 0:
+                        future = time.time() + 3
+                        while time.time() < future:
+                            RPL.servoWrite(0, 1000) # motors reverse
+                            RPL.servoWrite(2, 2000)
+                            if time.time() >= future:
+                                RPL.servoWrite(0, 0)
+                                RPL.servoWrite(2, 0)
+                        while RPL.digitalRead(16) == 1:
+                            while RPL.digitalRead(23) == 1:
+                                future = time.time() + 2
+                                while time.time() < future:
+                                    RPL.servoWrite(0, 0)
+                                    RPL.servoWrite(2, 2000)
+                                    if time.time() >= future:
+                                        RPL.servoWrite(0, 2000)
+                                        RPL.servoWrite(2, 1000)
+                            while RPL.digitalRead(23) == 0:
+                                while RPL.digitalRead(17) == 1:
                                     future = time.time() + 2
                                     while time.time() < future:
-                                        RPL.servoWrite(0, 0)
-                                        RPL.servoWrite(2, 2000)
+                                        RPL.servoWrite(0, 2000)
+                                        RPL.servoWrite(2, 0)
                                         if time.time() >= future:
                                             RPL.servoWrite(0, 2000)
                                             RPL.servoWrite(2, 1000)
-                                while RPL.digitalRead(23) == 0:
-                                    while RPL.digitalRead(17) == 1:
-                                        future = time.time() + 2
-                                        while time.time() < future:
-                                            RPL.servoWrite(0, 2000)
-                                            RPL.servoWrite(2, 0)
-                                            if time.time() >= future:
-                                                RPL.servoWrite(0, 2000)
-                                                RPL.servoWrite(2, 1000)
 while RPL.digitalRead(16) == 0:
     future = time.time() + 3
     while time.time() < future:
