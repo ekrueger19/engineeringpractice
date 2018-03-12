b00066ec8ff2fe0ef4c71017f5723fce0edc7e64
Amelia Backes <abackes19@bsmschool.org>
2018-03-12 09:48:36 -0500

    added left

--- evading2.py
+++ evading2.py
@@ -5,11 +5,14 @@ import time
 now = time.time()
 future = now
 
+# next step! add in minimum turning time
+
 motorL = 2
 motorR = 0
 
 right = 23
 front = 16
+left = 17
 
 
 RPL.servoWrite(motorR, 2000)
@@ -20,7 +23,10 @@ while True:
     RPL.servoWrite(motorR, 2000)
     RPL.servoWrite(motorL, 1000)
     print ".............."
-    while RPL.digitalRead(front) == 0: # ... turn until nothing
+    while RPL.digitalRead(front) == 0 and RPL.digitalRead(right) == 0 and RPL.digitalRead(left) == 0:
+        RPL.servoWrite(motorR, 1000)
+        RPL.servoWrite(motorL, 2000)
+    while RPL.digitalRead(front) == 0: # something ahead, turn until nothing
         RPL.servoWrite(motorL, 2000)
         print "++++++"
         if RPL.digitalRead(front) != 0: # nothing in front, go
@@ -30,9 +36,17 @@ while True:
             break
     while RPL.digitalRead(right) == 0: # something to right...
         print "llllllllllllll"
-        RPL.servoWrite(motorL, 2000)
+        RPL.servoWrite(motorL, 2000) # pivot
         if RPL.digitalRead(right) != 0: # nothing to side, go
             RPL.servoWrite(motorR, 2000)
             RPL.servoWrite(motorL, 1000)
             print ":::::::::::"
+            break
+    while RPL.digitalRead(left) == 0: # something to left...
+        print "ooooooooooo"
+        RPL.servoWrite(motorR, 1000) # pivot
+        if RPL.digitalRead(right) != 0: # nothing to side, go
+            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorL, 1000)
+            print "mmmmmmmmmmmmmm"
             break
