893bc4c1167c54fe3b3f89fe567de4270bc858ed
Amelia Backes <abackes19@bsmschool.org>
2018-03-14 10:51:34 -0500

    added some mininum turning times, I hope it works?

--- evading2.py
+++ evading2.py
@@ -5,6 +5,26 @@ import time
 now = time.time()
 future = now
 
+def Rmin(go)
+    future = go + 1
+    futureplus = future + 1
+    while go < future:
+        RPL.servoWrite(motorL, lgo)
+        RPL.servoWrite(motorR, rgo)
+    while go > future and go < futureplus:
+        RPL.servoWrite(motorL, rgo)
+        RPL.servoWrite(motorR, rgo)
+
+def Lmin(go)
+    future = go + 1
+    futureplus = future + 1
+    while go < future:
+        RPL.servoWrite(motorL, lgo)
+        RPL.servoWrite(motorR, rgo)
+    while go > future and go < futureplus:
+        RPL.servoWrite(motorL, lgo)
+        RPL.servoWrite(motorR, lgo)
+
 # next step! add in minimum turning time of ~= 1 second
 
 motorL = 0
@@ -25,12 +45,15 @@ while True:
     RPL.servoWrite(motorR, rgo)
     RPL.servoWrite(motorL, lgo)
     print ".............."
-    while RPL.digitalRead(front) == 0 and RPL.digitalRead(right) == 0 and RPL.digitalRead(left) == 0:
-        RPL.servoWrite(motorR, lgo)
-        RPL.servoWrite(motorL, rgo)
+    while RPL.digitalRead(front) == 0 and RPL.digitalRead(right) == 0: # reverse
+        if RPL.digitalRead(left) == 0:
+            RPL.servoWrite(motorR, lgo)
+            RPL.servoWrite(motorL, rgo)
     while RPL.digitalRead(front) == 0: # something ahead, turn until nothing
         RPL.servoWrite(motorL, rgo)
         print "++++++"
+        now = time.time()
+        Rmin(now)
         if RPL.digitalRead(front) != 0: # nothing in front, go
             RPL.servoWrite(motorR, rgo)
             RPL.servoWrite(motorL, lgo)
@@ -39,6 +62,8 @@ while True:
     while RPL.digitalRead(right) == 0: # something to right...
         print "llllllllllllll"
         RPL.servoWrite(motorL, rgo) # pivot
+        now = time.time()
+        Rmin(now)
         if RPL.digitalRead(right) != 0: # nothing to side, go
             RPL.servoWrite(motorR, rgo)
             RPL.servoWrite(motorL, lgo)
@@ -47,6 +72,8 @@ while True:
     while RPL.digitalRead(left) == 0: # something to left...
         print "ooooooooooo"
         RPL.servoWrite(motorR, lgo) # pivot
+        now = time.time()
+        Lmin(now)
         if RPL.digitalRead(right) != 0: # nothing to side, go
             RPL.servoWrite(motorR, rgo)
             RPL.servoWrite(motorL, lgo)
